fc1fa9678fa99169575fbd65caefb2426035a068
Damon Blais <damon@rethunk.tech>
2024-01-29 12:29:33 -0800

    chore(frontend): add initial Next.js configuration and project setup files

---
+++ frontend/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
+  // output: 'export', // Outputs a Single-Page Application (SPA).
+  reactStrictMode: true, // Enables React strict mode.
+}
+
+export default nextConfig
---
+++ frontend/src/styles/globalStyles.ts
@@ -0,0 +1,9 @@
+export const globalStyles = {
+  main: {
+    height: '100vh',
+    overflowX: 'hidden',
+    overflowY: 'auto',
+    padding: '0.5em 1em',
+  },
+  description: {},
+}
